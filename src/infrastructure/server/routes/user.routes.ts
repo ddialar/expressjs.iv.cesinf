a95f8f12ded4ee8b1554d317451c626c91abf683
ddialar <ddialar@gmail.com>
2020-11-30 16:59:59 +0000

    chore(step-3): Starting point.

--- src/infrastructure/server/routes/user.routes.ts
+++ src/infrastructure/server/routes/user.routes.ts
@@ -1,33 +1,20 @@
 import express, { Router } from 'express'
 
-import { getUserProfile, updateUserProfile } from '../../../domain'
+import { getUserProfile } from '../../../domain'
 import { UserDomainModel } from '../../../domain/models'
 
-import { NewUserProfileDto } from '../../dtos'
 import { ensureAuthenticated } from '../middlewares'
 import { RequestDto } from '../serverDtos'
 
 const userRoutes: Router = express.Router()
 
 userRoutes.get('/profile', ensureAuthenticated, async (req: RequestDto, res, next) => {
+  // TODO Retrieve the user ID from the request.
   const { id } = req.user as UserDomainModel
 
   try {
+    // TODO Request the profile
     res.json(await getUserProfile(id))
-  } catch (error) {
-    next(error)
-  }
-})
-
-userRoutes.put('/profile', ensureAuthenticated, async (req: RequestDto, res, next) => {
-  // TODO Retrieve the user ID from the request.
-  // const { id } = req.user as UserDomainModel
-  // TODO Retrieve the new profile data.
-  // const newProfileData = req.body as NewUserProfileDto
-
-  try {
-    // TODO Update the profile
-    // res.json(await updateUserProfile(id, newProfileData))
   } catch (error) {
     next(error)
   }

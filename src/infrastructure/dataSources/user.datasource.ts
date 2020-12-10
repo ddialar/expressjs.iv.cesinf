f67c1d48107d333c12f023903de1ca9159963b60
ddialar <ddialar@gmail.com>
2020-12-10 12:14:09 +0000

    refactor(user.datasource): Edit the 'updateUserById' and 'updateUserProfileById' method in order to only use the 'updateById' MongoDB method.

--- src/infrastructure/dataSources/user.datasource.ts
+++ src/infrastructure/dataSources/user.datasource.ts
@@ -1,6 +1,6 @@
 import { UserDomainModel, NewUserDomainModel, UserProfileDomainModel } from '../../domain/models'
 import { NewUserProfileDto, UpdateUserPayloadDto } from '../dtos'
-import { mapUserFromDtoToDomainModel } from '../mappers'
+import { mapUserFromDtoToDomainModel, mapUserFromDtoToProfileDomainModel } from '../mappers'
 import { mongodb } from '../orm'
 
 export const createUser = async (newUserData: NewUserDomainModel): Promise<void> => {
@@ -12,8 +12,7 @@ export const getUserByUsername = async (username: string): Promise<UserDomainMod
 export const getUserProfileById = async (userId: string): Promise<UserProfileDomainModel | null> =>
   mongodb.requests.user.getProfileById(userId)
 
-export const updateUserById = async (userId: string, updatedUserData: UpdateUserPayloadDto): Promise<void> => mongodb.requests.user.updateById(userId, updatedUserData)
-export const updateUserProfileById = async (userId: string, newUserProfileData: NewUserProfileDto): Promise<UserProfileDomainModel | null> => {
-  await mongodb.requests.user.updateProfileById(userId, newUserProfileData)
-  return await getUserProfileById(userId)
-}
+export const updateUserById = async (userId: string, updatedUserData: UpdateUserPayloadDto): Promise<UserDomainModel | null> =>
+  mapUserFromDtoToDomainModel(await mongodb.requests.user.updateById(userId, updatedUserData))
+export const updateUserProfileById = async (userId: string, newUserProfileData: NewUserProfileDto): Promise<UserProfileDomainModel | null> =>
+  mapUserFromDtoToProfileDomainModel(await mongodb.requests.user.updateById(userId, newUserProfileData))

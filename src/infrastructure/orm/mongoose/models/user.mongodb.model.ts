d2f47dd70a66d5326b4fd96031816ab1fabc0752
ddialar <ddialar@gmail.com>
2020-12-10 11:34:35 +0000

    fix(user.modngodb.model): Remove no valid field definition properties.

--- src/infrastructure/orm/mongoose/models/user.mongodb.model.ts
+++ src/infrastructure/orm/mongoose/models/user.mongodb.model.ts
@@ -5,13 +5,13 @@ const UserSchema = new Schema({
   username: { type: String, required: true, unique: true, immutable: true },
   password: { type: String, required: true },
   email: { type: String, required: true, immutable: true },
-  name: { type: String, default: null },
-  surname: { type: String, default: null },
-  avatar: { type: String, default: null },
-  token: { type: String, default: null },
+  name: { type: String, default: '' },
+  surname: { type: String, default: '' },
+  avatar: { type: String, default: '' },
+  token: { type: String, default: '' },
   enabled: { type: Boolean, required: true, default: true },
   deleted: { type: Boolean, required: true, default: false },
-  lastLoginAt: { type: String, default: null }
+  lastLoginAt: { type: String, default: '' }
 }, MONGO_SCHEMA_OPTIONS)
 
 export const User = mongoose.model('User', UserSchema)
